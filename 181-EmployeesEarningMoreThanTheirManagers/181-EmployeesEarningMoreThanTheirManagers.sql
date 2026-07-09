58cded26a6833a69516298d5c75aa2d5018834f0
Roshan <24243083@drngpit.ac.in>
2026-07-09 10:06:50 +0530

    Create 181-EmployeesEarningMoreThanTheirManagers

---
+++ 181-EmployeesEarningMoreThanTheirManagers/181-EmployeesEarningMoreThanTheirManagers.sql
@@ -0,0 +1,7 @@
+-- Last updated: 7/9/2026, 10:07:02 AM
+# Write your MySQL query statement below
+SELECT e1.name AS Employee
+FROM Employee e1
+JOIN Employee e2
+ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary;
