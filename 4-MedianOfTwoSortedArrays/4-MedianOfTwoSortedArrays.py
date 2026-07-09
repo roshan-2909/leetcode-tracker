2213a3dbbffe0c98571b28c68b0505bc90562612
Roshan <24243083@drngpit.ac.in>
2026-07-09 10:08:21 +0530

    Create 4-MedianOfTwoSortedArrays

---
+++ 4-MedianOfTwoSortedArrays/4-MedianOfTwoSortedArrays.py
@@ -0,0 +1,36 @@
+# Last updated: 7/9/2026, 10:08:33 AM
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        # Make nums1 the smaller array
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        m, n = len(nums1), len(nums2)
+        left, right = 0, m
+
+        while left <= right:
+            partitionX = (left + right) // 2
+            partitionY = (m + n + 1) // 2 - partitionX
+
+            maxLeftX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
+            minRightX = float('inf') if partitionX == m else nums1[partitionX]
+
+            maxLeftY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
+            minRightY = float('inf') if partitionY == n else nums2[partitionY]
+
+            if maxLeftX <= minRightY and maxLeftY <= minRightX:
+
+                # Even total length
+                if (m + n) % 2 == 0:
+                    return (
+                        max(maxLeftX, maxLeftY) +
+                        min(minRightX, minRightY)
+                    ) / 2.0
+
+                # Odd total length
+                return max(maxLeftX, maxLeftY)
+
+            elif maxLeftX > minRightY:
+                right = partitionX - 1
+            else:
+                left = partitionX + 1
