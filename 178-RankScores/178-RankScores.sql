02170aa0471b2c267212333490a4ffc9e4ffdc1a
Roshan <24243083@drngpit.ac.in>
2026-07-09 10:06:58 +0530

    Create 178-RankScores

---
+++ 178-RankScores/178-RankScores.sql
@@ -0,0 +1,7 @@
+-- Last updated: 7/9/2026, 10:07:10 AM
+# Write your MySQL query statement below
+SELECT
+    score,
+    DENSE_RANK() OVER (ORDER BY score DESC) AS `rank`
+FROM Scores
+ORDER BY score DESC;
