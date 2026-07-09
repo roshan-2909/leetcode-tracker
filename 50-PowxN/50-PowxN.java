cc197358a6a0050d1cc12938100d598992062ea5
Roshan <24243083@drngpit.ac.in>
2026-07-09 10:07:06 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 10:07:18 AM
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double result = 1;
+
+        while (N > 0) {
+            if ((N & 1) == 1)
+                result *= x;
+
+            x *= x;
+            N >>= 1;
+        }
+
+        return result;
+    }
+}
