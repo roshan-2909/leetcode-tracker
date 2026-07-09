835642e985633feb6c73bb275681b9ffde282eb9
Roshan <24243083@drngpit.ac.in>
2026-07-09 10:07:22 +0530

    Create 40-CombinationSumIi

---
+++ 40-CombinationSumIi/40-CombinationSumIi.java
@@ -0,0 +1,40 @@
+// Last updated: 7/9/2026, 10:07:34 AM
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(candidates, target, 0, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private void backtrack(int[] candidates, int target,
+                           int start, List<Integer> curr,
+                           List<List<Integer>> result) {
+
+        if (target == 0) {
+            result.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+
+            if (i > start && candidates[i] == candidates[i - 1])
+                continue;
+
+            if (candidates[i] > target)
+                break;
+
+            curr.add(candidates[i]);
+
+            backtrack(candidates,
+                    target - candidates[i],
+                    i + 1,
+                    curr,
+                    result);
+
+            curr.remove(curr.size() - 1);
+        }
+    }
+}
